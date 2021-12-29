3371051d33d2d8ae72884f4d5c6596590090ae81
Salvador Zaragoza <salvador.zaragoza@uabc.edu.mx>
2021-12-28 16:13:55 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <span>
+          {process.env.REACT_APP_ENV}
+        </span>
         <a
           className="App-link"
           href="https://reactjs.org"
